4c39ecbe5f2cd68e536d7ce047a8663ac4b4beea
opengl-8080 <opengl-8080@users.noreply.github.com>
2023-01-23 16:03:40 +0900

    feat: javax -> jakarta

--- src/test/java/nablarch/common/permission/SystemAccountAuthority.java
+++ src/test/java/nablarch/common/permission/SystemAccountAuthority.java
@@ -1,9 +1,9 @@
 package nablarch.common.permission;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 /**
  *　SYSTEM_ACCOUNT_AUTHORITY
